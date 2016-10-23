25caf62bc4da256d059b603222a508a05684c5fe
jacekk <gitkraken@jacek-k.net>
2016-10-24 01:35:13 +0200

    fixed router error on hot-reload;

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,26 +7,26 @@ import PlayerPage from '../containers/PlayerPage';
 import theme from '../material_ui_raw_theme_file';
 import { history } from '../store';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import React from 'react';
 import { Component } from 'react';
 import { IndexRoute, Route, Router } from 'react-router';
 
-// For Customization Options, edit  or use
-// './src/material_ui_raw_theme_file.jsx' as a template.
+const routerConfig = (
+    <Route path="/" component={AppLayout}>
+        <IndexRoute component={HomePage} />
+        <Route path="collections/:collectionId/play" component={PlayerPage} />
+        <Route path="collections/:collectionId" component={LinksPage}>
+            <Route path="link" component={NewLinkContainer} />
+            <Route path="link/:linkId" component={LinkEditContainer} />
+        </Route>
+    </Route>
+);
 
 class App extends Component {
     render() {
         return (
             <MuiThemeProvider muiTheme={theme}>
-                <Router history={history}>
-                    <Route path="/" component={AppLayout}>
-                        <IndexRoute component={HomePage} />
-                        <Route path="collections/:collectionId/play" component={PlayerPage} />
-                        <Route path="collections/:collectionId" component={LinksPage}>
-                            <Route path="link" component={NewLinkContainer} />
-                            <Route path="link/:linkId" component={LinkEditContainer} />
-                        </Route>
-                    </Route>
-                </Router>
+                <Router history={history} routes={routerConfig} />
             </MuiThemeProvider>
         );
     }
