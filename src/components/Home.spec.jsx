01da4c5086daaf1ae816fe9c3ff5510270a5ea34
jacekk <github.com@jacek-k.net>
2017-01-28 01:00:57 +0100

    added one more test for Home component;

--- src/components/Home.spec.jsx
+++ src/components/Home.spec.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 
-import { wrap, renderInDiv, mockActions } from '../../tests/utils';
+import { renderInDiv, mount, mockActions } from '../../tests/utils';
 import Home from './Home';
 
 const actions = mockActions('newCollectionDialog');
 
 describe('Home', () => {
     it('renders without crashing', () => {
-        renderInDiv(wrap(<Home
+        renderInDiv(<Home
                 noCollections={true}
                 actions={actions}
-        />));
+        />);
     });
+
+    it('renders message if there are no collections', () => {
+        const home = mount(<Home
+                noCollections={true}
+                actions={actions}
+        />);
+
+        expect(home.text()).toContain('There are no collections to play with.');
+    })
 });
--- tests/utils.js
+++ tests/utils.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { mount as enzymeMount } from 'enzyme';
 import theme from '../src/material_ui_raw_theme_file';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+export const mount = (component) => enzymeMount(wrapWithTheme(component));
+
 export const renderInDiv = (component) => ReactDOM.render(
-    component,
+    wrapWithTheme(component),
     document.createElement('div')
 );
 
-export const wrap = (component) => {
+export const wrapWithTheme = (component) => {
     return <MuiThemeProvider muiTheme={theme}>
         {component}
     </MuiThemeProvider>;
